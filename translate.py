0e55335a315e9ed2dbc388c576d368b26c729325
montero-melis <montero.gui@gmail.com>
2019-04-01 17:30:56 +0200

    Makes the chunking of the verb list more idiomatic and adds a further suggestion on how to do this

--- translate.py
+++ translate.py
@@ -2,7 +2,6 @@
 
 from googletrans import Translator
 import csv
-import numpy
 
 
 # read in Dutch verbs
@@ -26,20 +25,23 @@ csv_writer.writerow(columns)
 
 # There is a limit to the number of requests to Google translate, see
 # https://cloud.google.com/translate/quotas
-# Slice therefore the list into smaller chunks and save to disk.
-# (NB: I can't figure out a simple way of subsetting lists!!)
+# Break therefore down the list into smaller sublists and save to disk.
+# (NB: See bottom of file for alternative ways of doing this)
 steps_of = 20
 index_list = [x*steps_of for x in range(1 + len(du_verbs)//20)]
 
-for i in index_list:
+du_verbs_chunked = [
+	du_verbs[x*steps_of : (x+1) * steps_of]
+		for x in range(1 + len(du_verbs)//20)
+	]
+
+for verb_sublist in du_verbs_chunked:
 	# initialize list for translations
 	translated_verbs = []
-	# current subset of Dutch verbs to be translated
-	curr_verbs = du_verbs[i : i+steps_of]
-	for word in curr_verbs:
+	for word in verb_sublist:
 		for lang in languages:
-			translated_verbs.append(
-				[word,
+			translated_verbs.append([
+				word,
 				lang#,
 				# translator.translate(word, dest = lang).text
 				])
@@ -48,3 +50,20 @@ for i in index_list:
 		csv_writer.writerow(item)
 
 outfile.close()
+
+
+
+#################################################
+# Alessio Quaresima showed me this alternative way of looping through a list
+# using [Python generators](https://wiki.python.org/moin/Generators), which
+# avoid storing big chunks in memory (not a problem here but could become an
+# issue when dealing with bigger data sets)
+#################################################
+
+# HAVEN'T RUN THIS CODE (I think it's incomplete, but the gist is there?):
+
+# def gener_short_sample(du_verbs):
+#     for x in range(len(du_verbs)//20):
+#         yield new_du_verbs[x*steps_of:(x+1)*steps_of]
+
+# my_generator = gener_short_sample(du_verbs)
