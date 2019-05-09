9136c27bfb7dd1cee66183800f028e90b5561310
Guillermo Montero-Melis <guimon@mpi.nl>
2019-05-09 18:04:39 +0200

    Adds a script to generate stimuli in appropriate xml format -- and the thus generated stimuli

---
+++ online_exp/create_xml_stimuli.R
@@ -0,0 +1,67 @@
+## A script to generate the stimuli objects for the MPI frinex experiment
+## platform, so I can simply copy and paste
+## (NB: this is very specific to that experiment and just saves me from typing)
+
+
+## read verb stimuli
+verbs <- read.csv("online_exp/verbs_pilot.csv", fileEncoding = "UTF-8")
+verbs$id <- 1 : nrow(verbs)
+verbs
+
+
+## create output file
+file.create("online_exp/stimuli_xml.txt")
+
+
+## feature task
+feature_xml <- paste(
+  '<stimulus identifier="feature',
+  verbs$id,
+  '" label="',
+  verbs$feature,
+  '" pauseMs="0" tags="feature_task"/>',
+  sep = "")
+head(feature_xml)
+
+# Write to disk
+cat("<!-- stimuli for FEATURE task -->", file = "online_exp/stimuli_xml.txt",
+    sep = "\n", append = TRUE)
+write.table(feature_xml, file = "online_exp/stimuli_xml.txt", quote = FALSE,
+            col.names = FALSE, row.names = FALSE, append = TRUE)
+cat("\n", file = "online_exp/stimuli_xml.txt", append = TRUE)
+
+
+## negation task
+negation_xml <- paste(
+  '<stimulus identifier="negation',
+  verbs$id,
+  '" label="Zij ',
+  verbs$negation,
+  ', maar zij..." pauseMs="0" tags="negation_task"/>',
+  sep = "")
+negation_xml
+
+# Write to disk
+cat("<!-- stimuli for NEGATION task -->", file = "online_exp/stimuli_xml.txt",
+    sep = "\n", append = TRUE)
+write.table(negation_xml, file = "online_exp/stimuli_xml.txt", quote = FALSE,
+            col.names = FALSE, row.names = FALSE, append = TRUE)
+cat("\n", file = "online_exp/stimuli_xml.txt", append = TRUE)
+
+
+## via task
+via_xml <- paste(
+  '<stimulus identifier="via',
+  verbs$id,
+  '" label="Zij ',
+  verbs$via,
+  ' via ..." pauseMs="0" tags="via_task"/>',
+  sep = "")
+via_xml
+
+# Write to disk
+cat("<!-- stimuli for VIA task -->", file = "online_exp/stimuli_xml.txt",
+    sep = "\n", append = TRUE)
+write.table(via_xml, file = "online_exp/stimuli_xml.txt", quote = FALSE,
+            col.names = FALSE, row.names = FALSE, append = TRUE)
+cat("\n", file = "online_exp/stimuli_xml.txt", append = TRUE)
